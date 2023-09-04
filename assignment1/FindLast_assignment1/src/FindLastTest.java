029f9ebd130c71dddbb82be399c92cf88c7dfadb
RaymondSWE <RamanMohammed@hotmail.com>
2023-09-04 19:09:06 +0200

    added assignment 1

---
+++ assignment1/FindLast_assignment1/src/FindLastTest.java
@@ -0,0 +1,21 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class FindLastTest
+{
+   // this test fails!
+   @Test public void lastOccurrenceInFirstElement() 
+   {
+      int arr[] = {2, 3, 5};
+      int y = 2;
+      assertEquals("Last occurence in first element", 0, FindLast.findLast(arr, y));
+   }
+
+   // ----------------------------------------------------------------
+   // -----------------------------------------------------------------
+   // Add your tests below with a comment stating which question it belongs to
+   
+}
+

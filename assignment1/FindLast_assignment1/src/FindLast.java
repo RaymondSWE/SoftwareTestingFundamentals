75f2ae624341deb124c9f0ba5d4e03e29f693879
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-09-05 14:46:23 +0200

    test added

--- assignment1/FindLast_assignment1/src/FindLast.java
+++ assignment1/FindLast_assignment1/src/FindLast.java
@@ -12,14 +12,17 @@ public class FindLast {
 
 	public static int findLast (int[] x, int y)
 	   { 
-	      for (int i=x.length-1; i > 0; i--)
+	      for (int i=x.length - 1; i >= 0; i--)
 	      {
 	         if (x[i] == y) 
 	         {
 	            return i;
+	           
 	         }
+		     System.out.println(x[i]);
 	      }
 	      return -1;
+
 	   }
 
 	public static void main(String[] argv) {
--- assignment1/FindLast_assignment1/src/FindLastTest.java
+++ assignment1/FindLast_assignment1/src/FindLastTest.java
@@ -8,7 +8,7 @@ public class FindLastTest
    // this test fails!
    @Test public void lastOccurrenceInFirstElement() 
    {
-      int arr[] = {2, 3, 5};
+      int arr[] = { 2, 3, 5};
       int y = 2;
       assertEquals("Last occurence in first element", 0, FindLast.findLast(arr, y));
    }
@@ -16,6 +16,32 @@ public class FindLastTest
    // ----------------------------------------------------------------
    // -----------------------------------------------------------------
    // Add your tests below with a comment stating which question it belongs to
+   
+   @Test
+   public void findFirstElement() {
+      int arr[] = {2, 3, 5};
+      int y = 3;
+      assertEquals("Find first element in array", 1, FindLast.findLast(arr, y));
+   }
+   
+   @Test public void emptyArraySearch() 
+   {
+      int arr[] = {};
+      int y = 2;
+      assertEquals("Empty array search", -1, FindLast.findLast(arr, y));
+   }
+   
+   
+   @Test (expected = NullPointerException.class)
+   public void searchForNull() 
+   {
+      int arr[] = null;
+      int y = 2;
+      assertEquals("Empty array search", null, FindLast.findLast(arr, y));
+   }
+   
+
+
    
 }
 

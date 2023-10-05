feb6c52f42b4ad4bc7cd974974e285a958057a08
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-10-05 20:08:36 +0200

    RACC test cases added for predicate 3

--- assignment2/src/predicateRACC/PredicateThreeRACC.java
+++ assignment2/src/predicateRACC/PredicateThreeRACC.java
@@ -10,7 +10,53 @@ import cal.Cal;
 public class PredicateThreeRACC {
 	
  
+    //testing when year is inside boundries (TT)
+    @Test
+    public void TC_11() {
+        int year = 1;
+        int month1 = 1;
+        int month2 = 1;
+        int day1 = 1;
+        int day2 = 2;
+        int result = Cal.cal(month1, day1, month2, day2, year);
+        System.out.println("the result when testing year is" + result);
+        assertEquals("testing year=1", 1, result);
+    }
+	
+    //testing for when year is > 10000 (FT)
+    @Test
+    public void TC_12() {
+        try {
+            int year = 10001;
+            int month1 = 1;
+            int month2 = 1;
+            int day1 = 1;
+            int day2 = 2;
+            int result = Cal.cal(month1, day1, month2, day2, year);
+            fail("error on year");
+        } catch (IllegalArgumentException e) {
+            assertEquals("year must be <10001", e.getMessage());
 
+        }
+    }
+	
+	
+    //testing for when year is <1 (TF)
+    @Test
+    public void TC_13() {
+        try {
+            int year = 0;
+            int month1 = 1;
+            int month2 = 1;
+            int day1 = 1;
+            int day2 = 2;
+            int result = Cal.cal(month1, day1, month2, day2, year);
+            fail("error on year");
+        } catch (Exception e) {
+            assertEquals("year must be >0", e.getMessage());
+
+        }
+    }
 	
 
 }

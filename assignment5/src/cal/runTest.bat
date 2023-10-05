c1594898043a31c27bdad037928e18c43a6c5f4f
nibarsito <nibarsito2@gmail.com>
2023-10-05 12:30:16 +0200

    minor changes to some files and added a CalTest.java file.

--- assignment5/src/cal/runTest.bat
+++ assignment5/src/cal/runTest.bat
@@ -1,1 +1,1 @@
-java -cp "junit-4.13.1.jar;hamcrest-2.2.jar;FindLast.jar;." org.junit.runner.JUnitCore FindLastTest
+java -cp "junit-4.13.1.jar;hamcrest-2.2.jar;Cal.jar;." org.junit.runner.JUnitCore CalTest

233bbd2fb3ab61a2549c42653bf5233ff5784c8e
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-10-05 20:05:03 +0200

    added RACC test case file for predicate 3

---
+++ assignment2/src/predicateRACC/PredicateThreeRACC.java
@@ -0,0 +1,16 @@
+package predicateRACC;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import cal.Cal;
+
+public class PredicateThreeRACC {
+	
+ 
+
+	
+
+}
